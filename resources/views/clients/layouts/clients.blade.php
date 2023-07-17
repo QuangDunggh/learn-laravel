42621cbc9626232de488bf12daf71f8a5aaaaaf4
Quang Dung <dphanquang@gmail.com>
2023-07-17 18:10:24 +0700

    'first-add'

---
+++ resources/views/clients/layouts/clients.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('assets/clients/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/clients/css/style.css')}}">
+    <title>Document</title>
+</head>
+<body>
+    @include('clients.partials.header')
+    <main>
+        <aside>
+
+        </aside>
+
+        <div class="content">
+
+        </div>
+    </main>
+
+    <footer>
+        <h1>FOOTER</h1>
+    </footer>
+</body>
+
+<script src="{{asset('assets/clients/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/clients/js/custom.min.js')}}"></script>
+</html>
